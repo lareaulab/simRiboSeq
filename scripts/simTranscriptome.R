635b088c7a60fbfd58d61ce23358b45ec863e81d
Amanda Mok <a.mok@berkeley.edu>
2019-03-18 19:53:22 -0700

    simulate human transcriptome

--- scripts/simTranscriptome.R
+++ scripts/simTranscriptome.R
@@ -52,5 +52,40 @@ names(randomCodonDist) <- codons
 yeastGenes <- simTranscriptome(yeastGeneLengths, yeastCodonDist, 
                                file.path("outputs", "simYeast.fa"))
 # simulate random transcriptome: yeast gene lengths, random codon usage
-randomGenes <- simTranscriptome(yeastGeneLengths, randomCodonDist, 
-                                file.path("outputs", "simRandom.fa"))
+randomGenes_yeast <- simTranscriptome(yeastGeneLengths, randomCodonDist, 
+                                file.path("outputs", "simRandom_yeast.fa"))
+
+### human genome
+humanGeneLengths <- read.table("refData/gencode.v22.transcript.13cds10.lengths.txt", header=F)[,3]/3
+humanFAfile <- readLines("refData/gencode.v22.transcript.13cds10.fa")
+humanGeneCodons <- lapply(grep(">", humanFAfile),
+                          function(x) {
+                            sequence <- humanFAfile[x+1]
+                            nCodons <- floor(nchar(sequence)/3)
+                            # account for 13cds10
+                            codons <- substring(sequence, first=(3*(1:nCodons)-1), last=(3*(1:nCodons)+1))
+                            # 0-based indexing
+                            names(codons) <- as.character(seq.int(from=-4, length.out=nCodons))
+                            return(codons)
+                          })
+# codons <- apply(expand.grid(c("A", "T", "C", "G"),
+#                             c("A", "T", "C", "G"),
+#                             c("A", "T", "C", "G")),
+#                 1, paste, collapse="")
+humanCodonCounts <- sapply(humanGeneCodons,
+                           function(gene) {
+                             gene_trim <- gene[5:(length(gene)-4)]
+                             sapply(codons, 
+                                    function(codon) {
+                                      sum(gene_trim==codon)
+                                    })
+                           })
+humanCodonDist <- rowSums(humanCodonCounts)/sum(humanCodonCounts)
+# randomCodonDist <- rep(1, length(codons))/length(codons)
+# names(randomCodonDist) <- codons
+# simulate human transcriptome: human gene lengths, human codon usage
+humanGenes <- simTranscriptome(humanGeneLengths, humanCodonDist, 
+                               file.path("outputs", "simhuman.fa"))
+# simulate random transcriptome: human gene lengths, random codon usage
+randomGenes_human <- simTranscriptome(humanGeneLengths, randomCodonDist, 
+                                file.path("outputs", "simRandom_human.fa"))
