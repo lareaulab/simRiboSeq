ad61ef2895ebcae44da508e1523ecb2cf03b9d62
Amanda Mok <a.mok@berkeley.edu>
2021-01-11 11:25:57 -0800

    align simulated riboviz data using hisat2 ; re-order footprint counts in simRiboviz_footprint_counts.txt

--- simulation_runs/riboviz/process_files.sh
+++ simulation_runs/riboviz/process_files.sh
@@ -9,6 +9,11 @@ bowtie-build yeast_YAL_CDS_w_250utrs.fa yeast_YAL_CDS_w_250utrs
 bowtie -a --norc -v 2 -p 20 yeast_YAL_CDS_w_250utrs simRiboviz_trimmed.fq \
     > simRiboviz_footprints.bam 2> simRiboviz_footprints.bowtiestats
 
+hisat2-build yeast_YAL_CDS_w_250utrs.fa yeast_YAL_CDS_w_250utrs
+hisat2 -k 2 --no-spliced-alignment --rna-strandness F --no-unal \
+	-x yeast_YAL_CDS_w_250utrs -S simRiboviz_footprints.sam -U simRiboviz_trimmed.fq
+
 # 3. zip files for github
 gzip -k simRiboviz.fq
 gzip -k simRiboviz_footprints.bam
+gzip -k simRiboviz_footprints.sam
